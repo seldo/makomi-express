18f411a514ca0871041e2c9e13850bbd021742ac
Laurie <github@seldo.com>
2013-06-13 19:07:26 -0700

    Basic no-transform parser for MEJS. HTML output from view generator.

--- generators/views.js
+++ generators/views.js
@@ -20,22 +20,172 @@ exports.generate = function() {
  * flat views. In fact, feel free to generate a templating language that
  * understands MEJS natively, and then this whole step can just be a file copy.
  */
-exports.expand = function(filename,cb) {
+exports.parseFile = function(filename,cb) {
 
   var htmlparser = require("htmlparser");
   var fs = require('fs');
-  var util = require('util');
 
   console.log("Expanding " + filename)
 
   fs.readFile(filename,function(er,rawHtml) {
+    // this is the callback when the parsing is done
     var handler = new htmlparser.DefaultHandler(function (er, dom) {
-      console.log("parsing done, I think?")
-      cb()
+      console.log("Before:")
+      console.log(dom);
+      exports.translateDom(handler.dom,function(translated,payloads) {
+        // payloads should be null at this level
+        // FIXME: no error handling yet
+        cb(er,translated)
+      })
     });
     var parser = new htmlparser.Parser(handler);
     parser.parseComplete(rawHtml);
-    console.log(util.inspect(handler.dom, false, null));
+  })
+
+}
+
+/**
+ * Reads every sibling element at the current level of the dom
+ * Transforms them as necessary
+ * Recurses down into any children
+ * Callback is the translated structure, plus anything else that needs to get passed up the tree
+ * @param dom
+ * @param cb
+ */
+exports.translateDom = function(dom,cb) {
+  // the translated dom
+  var translated = []
+  // content extracted from this level to be passed to yiel
+  var payloads = {}
+
+  // counter+callback idiom
+  var handled = dom.length
+  var isHandled = function() {
+    handled--;
+    if (handled == 0) {
+      cb(translated,payloads)
+    }
+  }
+
+  dom.forEach(function(element,index) {
+    switch(element.type) {
+      case "comment":
+        element.raw = "COMMENT:" + element.raw
+        translated.push(element)
+        isHandled()
+        break;
+      case "tag":
+        // handle children. Can I really do this out of sync without going crazy?
+        if (element.children) {
+          handled++
+          exports.translateDom(element.children,function(translated,payloads) {
+            element.children = translated
+            // TODO: payload handling
+            isHandled()
+          })
+        }
+        // decide what to do with the element itself
+        switch(element.name) {
+          case "parent":
+            translated.push(element)
+            console.log("ignored parent " + element.raw)
+            isHandled()
+            break;
+          case "child":
+            // TODO: translate and expand the child file
+            // FIXME: parseRoot should be relative to the first file passed in
+            var parseRoot = "./test/data/testapp/views/"
+            // TODO: verify attribs.src exists
+            /*
+            exports.parseFile(parseRoot+element.attribs.src,function(er,childDom) {
+              translated.con(childDom)
+              console.log("pushed stack resulting from parsing " + element.raw)
+              isHandled()
+            })
+            */
+            translated.push(element);
+            isHandled();
+            break;
+          case "payload":
+            /*
+             Baby, I can see your payload
+             You know you're my saving grace
+             You're everything I need and more
+             It's written all over your face
+             */
+            // TODO: push payloads up the stack
+            translated.push(element)
+            console.log("ignored payload " + element.raw)
+            isHandled()
+            break;
+          default:
+            // leave unrecognized tags unchanged
+            translated.push(element)
+            console.log("ignored unknown tag " + element.raw)
+            isHandled()
+            break;
+        }
+        break;
+      default:
+        // leave unrecognized element types unchanged
+        translated.push(element)
+        isHandled()
+        break;
+    }
+  })
+}
+
+/**
+ * Take our formatted dom structure and output actual HTML
+ * @param dom
+ * @param cb
+ * @param depth
+ */
+exports.toHtml = function(dom,cb,depth) {
+  if (!depth) depth = 0;
+
+  // counter+callback idiom
+  var handled = dom.length
+  var isHandled = function() {
+    handled--;
+    if (handled == 0) {
+      cb(output)
+    }
+  }
+
+  var output = "";
+
+  dom.forEach(function(element,index) {
+    switch(element.type) {
+      case "comment":
+        output += "<!-- " + element.raw + " -->"
+        isHandled()
+        break;
+      case "tag":
+        output += "<" + element.raw + ">"
+        var endTag = function() {
+          if (element.name != '%=') {
+            output += "</" + element.name + ">"
+          }
+        }
+        if (element.children) {
+          handled++
+          exports.toHtml(element.children,function(html) {
+            output += html
+            endTag()
+            isHandled()
+          },depth+2)
+          isHandled()
+        } else {
+          endTag()
+          isHandled()
+        }
+        break;
+      default:
+        output += element.raw
+        isHandled()
+        break;
+    }
   })
 
 }
--- test/views.js
+++ test/views.js
@@ -5,13 +5,19 @@
 var test = require('tape');
 var generator = require('../generators/views.js');
 var fs = require('fs');
+var util = require('util');
 
-test('expand a single template tree', function (t) {
+test('parse a single template chain', function (t) {
 
   var filename = "./test/data/testapp/views/index.mejs"
 
-  generator.expand(filename,function() {
+  generator.parseFile(filename,function(er,dom) {
     console.log("Generator expanded " + filename)
+    console.log(util.inspect(dom,false,null))
+    generator.toHtml(dom,function(html) {
+      console.log(html);
+    });
+
   });
 
   t.end() // fuck this no tests
