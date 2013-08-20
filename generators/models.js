9daa04a324e7caae085a4167488e0df8e946f18c
Laurie <github@seldo.com>
2013-08-19 18:57:30 -0700

    Beginning model generation

--- generators/models.js
+++ generators/models.js
@@ -2,68 +2,58 @@ var fs = require('fs-extra');
 var _ = require('underscore');
 var mkSrc = require('makomi-source-util')
 
+/**
+ * Given a model directory and an output directory, generate model
+ * classes for all the models in an app and write them to disk.
+ * @param rootDir
+ * @param outputDir
+ * @param devMode
+ * @param cb
+ */
 exports.generate = function(rootDir, outputDir, devMode, cb) {
 
-  // TODO: foreach model in rootdir, generate a model class
+  // generate a model class for each model we find
+  mkSrc.files.listJSON(rootDir,function(er,modelNames) {
+
+    var count = modelNames.length;
+    if (modelNames.length == 0) cb()
+    var complete = function() {
+      count--
+      if (count==0) {
+        console.log("All models generated")
+        cb()
+      }
+    }
+
+    modelNames.forEach(function(modelName,index) {
+      exports.createModel(rootDir,modelName,function(modelCode) {
+        mkSrc.files.writeToDir(outputDir,modelName+'.js',modelCode,function() {
+          console.log("Generated " + modelName)
+          complete();
+        })
+      })
+    })
+  })
 
 }
 
-exports.createModel = function(rootDir, model, outputDir, cb) {
+/**
+ *
+ * @param rootDir
+ * @param model
+ * @param outputDir
+ * @param cb
+ */
+exports.createModel = function(rootDir, modelName, cb) {
 
   // TODO: find the adapters required
   // TODO: foreach query, translate the queries into methods
 
+  fs.readJSONFile(rootDir + modelName,function(er,model) {
+  })
+
 }
 
 exports.createQuery = function(model,controller,action,cb) {
-
-}
-
-// TODO: move these to util, and refactor controllers to use them
-
-
-
-// TODO: replace with readJSONFile, writeJSONFile, etc.
-
-/**
- * Read in the file. Maybe also validate it here in future.
- * @param routingFile
- * @param cb
- */
-exports.read = function (file, cb) {
-  fs.readFile(file, 'utf-8', cb);
-}
-
-/**
- * Trivial to parse because it's JSON. Some validation a good idea in future.
- * @param routerString
- * @param cb
- */
-exports.parse = function (fileString, cb) {
-  var parsed = JSON.parse(fileString);
-  // FIXME: catch errors, send them instead of always null
-  var er = null;
-  cb(er, parsed);
-}
-
-/**
- * Given a file "object", which contains a name and the file body, write that file
- * We are given the output dir for all these.
- * We may need some logic here to handle wacky/erroneous output locations/names.
- * @param fileObject
- * @param outputDir
- * @param cb
- */
-exports.write = function (dir, name, body, cb) {
-
-  var path = dir + name
-  fs.writeFile(path, body, function (er) {
-    if (er) {
-      console.log(er);
-    } else {
-      console.log("Wrote " + path);
-    }
-    cb(er);
-  });
 
 }
